6fa0b957887b54b28e329ce81f6254b4fe24bb9d
Artur Balabanov <arturbalabanov97@gmail.com>
2024-11-29 11:50:29 +0000

    Add broken test (and linting fail too :)) to test the pipeline

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,7 @@
 async def test_has_openapi_json(api_client):
     response = await api_client.get("/openapi.json")
     assert response.status_code == 200
+
+
+async def test_failure():
+    assert False
